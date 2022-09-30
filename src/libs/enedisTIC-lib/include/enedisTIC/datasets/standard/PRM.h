e6a957eecd4487650ee15f71ada22f5e3f5c1d3b
Aloïké <aloike.go@gmail.com>
2022-09-30 12:30:26 +0200

    Datasets::PRM: Ajout de doc.

--- src/libs/enedisTIC-lib/include/enedisTIC/datasets/standard/PRM.h
+++ src/libs/enedisTIC-lib/include/enedisTIC/datasets/standard/PRM.h
@@ -19,7 +19,18 @@ namespace Datasets {
 /* ########################################################################## */
 /* ########################################################################## */
 /**
- *  @brief  PRM.
+ *  @brief  PRM (Point de Référence des Mesures).
+ *
+ *  Ce numéro correspond à la référence de votre logement ou de votre local ;
+ *  c’est votre référence client.
+ *  Composé de 14 chiffres il figure sur la facture d’électricité de votre
+ *  fournisseursous ce nom : numéro de PDL ou PRM.
+ *  Il apparaît en haut à droite de votre facture d’électricité.
+ *  Si vous contactez votre fournisseur d’électricité ou Enedis, pensez à vous
+ *  munir de ce numéro à 14 chiffres, il permet de vous prendre en charge plus
+ *  rapidement.
+ *  PRM signifie Point de Référence des Mesures,
+ *  PDL signifie Point De Livraison. C’est la même référence à 14 chiffres.
  */
 class   LIBENEDISTIC_EXPORT PRM
         :   public  GenericDataset
